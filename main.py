80964a62860d942c78da3d2f5ae2f49ce6c01e6c
Wenxuan Shen <bushigemen114@gmail.com>
2025-12-22 23:24:09 +0800

    特殊教育 (#2)
    * 特殊教育
    
    * 修复book判定不到的问题
    
    * 合并到special.json
    
    * chore: 更新列表
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- main.py
+++ main.py
@@ -140,3 +140,112 @@ for period in periods[:-1]: # 不处理特殊教育的数据
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(final_data, f, ensure_ascii=False, indent=4)
     logger.success(f"已写入文件 {filename}")
+
+categories = []
+for category in periods[-1]["hierarchies"][0]["children"]: # 处理特殊教育的数据
+    category_data = {
+        "name": category["tag_name"],
+        "periods": []
+    }
+    category_id = category["tag_id"]
+    logger.debug(f"正在遍历特殊教育类别 {name}。ID: {category_id}")
+
+    origindata_periods = category["hierarchies"][0]["children"]
+    if category["hierarchies"][0]["hierarchy_name"] == "学科":
+        # 创建形式学段 特殊教育
+        origindata_periods = [{"tag_id": "", "tag_name": "全部", "hierarchies": category["hierarchies"]}]
+    for period in origindata_periods:
+        period_data = {
+            "name": period["tag_name"],
+            "subjects": []
+        }
+        period_id = period["tag_id"]
+        if period_id == "":
+            logger.debug(f"正在遍历特殊教育形式学段 全部。没有 ID。")
+        else:
+            logger.debug(f"正在遍历特殊教育学段 {period_data['name']}。ID: {period_id}")
+
+        origindata_subjects = period["hierarchies"][0]["children"]
+        for subject in origindata_subjects:
+            subject_data = {
+                "name": subject["tag_name"],
+                "grades": []
+            }
+            subject_id = subject["tag_id"]
+            logger.debug(f"正在遍历学科 {subject_data['name']}。ID: {subject_id}")
+
+            origindata_grades = subject["hierarchies"][0]["children"]
+            if subject["hierarchies"][0]["hierarchy_name"] == "册次":
+                # 创建形式年级 全部
+                origindata_grades = [{"tag_id": "", "tag_name": "全部", "hierarchies": subject["hierarchies"]}]
+            for grade in origindata_grades:
+                grade_data = {
+                    "name": grade["tag_name"],
+                    "volumes": []
+                }
+                grade_id = grade["tag_id"]
+                if grade_id == "":
+                    logger.debug(f"正在遍历形式年级 全部。没有 ID。")
+                else:
+                    logger.debug(f"正在遍历年级 {grade_data['name']}。ID: {grade_id}")
+                
+                origindata_volumes = grade["hierarchies"][0]["children"]
+                if origindata_volumes == []:
+                    # 创建形式册次 全部
+                    origindata_volumes = [{"tag_id": "", "tag_name": "全部"}]
+                for volume in origindata_volumes:
+                    volume_data = {
+                        "name": volume["tag_name"],
+                        "books": []
+                    }
+                    volume_id = volume["tag_id"]
+                    if volume_id == "":
+                        logger.debug(f"正在遍历形式册次 全部。没有 ID。")
+                    else:
+                        logger.debug(f"正在遍历册次 {volume_data['name']}。ID: {volume_id}")
+                    books = []
+                    for book in tag_data:
+                        if "tag_paths" not in book.keys():
+                            continue
+                        if book["tag_paths"] == []:
+                            continue
+                        # 更傻福的判断
+                        if f"{category_id}{("/" + period_id) if period_id != "" else ""}/{subject_id}{("/" + grade_id) if grade_id != "" else ""}{("/" + volume_id) if volume_id != "" else ""}" in book["tag_paths"][0]:
+                            try:
+                                ti_response = requests.get(
+                                    f"https://{IP}/zxx/ndrv2/resources/tch_material/details/{book["id"]}.json",
+                                    headers={"Host": DOMAIN},
+                                    verify=False
+                                ).json()
+                                for ti in ti_response["ti_items"]:
+                                    if ti["ti_storage"].endswith(".pdf"):
+                                        path = ti["ti_storage"].replace("cs_path:${ref-path}", "")
+                                        break
+                                book_data = {
+                                    "name": book["title"],
+                                    "content_id": book["id"],
+                                    "path": path
+                                }
+                            except:
+                                book_data = {
+                                    "name": book["title"],
+                                    "content_id": book["id"],
+                                    "path": f"{book["id"]}: Failed to fetch path"
+                                }
+                            books.append(book_data)
+                            logger.success(f"匹配到课本 {book_data['name']}，ID: {book["id"]}")
+                    volume_data["books"] = books
+                    grade_data["volumes"].append(volume_data)
+                subject_data["grades"].append(grade_data)
+            period_data["subjects"].append(subject_data)
+        category_data["periods"].append(period_data)
+    categories.append(category_data)
+final_data = {
+    "version": datetime.now().strftime("%Y%m%d%H%M"),
+    "categories": categories
+}
+filename = "special.json"
+with open(filename, "w", encoding="utf-8") as f:
+    json.dump(final_data, f, ensure_ascii=False, indent=4)
+logger.success(f"已写入文件 {filename}")
+            
